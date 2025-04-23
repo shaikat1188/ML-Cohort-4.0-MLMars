7cc6c92f1b4a24a47ea6d12a1d42b8a911834ff6
Shaikat Rahman Khan <rahmanshaikat613@gmail.com>
2025-04-24 01:18:59 +0600

    Add files via upload

---
+++ TASKS/task#1.py
@@ -0,0 +1,24 @@
+#Q1
+x='ABRACADABRA'
+print(x[2])
+
+#Q2
+X='The quick brown fox jumps over the lazy dog'
+print(X[10:20])
+
+#Q3
+'''
+A= 'বাংলা '
+B= 'আমার '
+C= 'সোনার '
+print(B+C+A) Bangla fonts not showing correctly for some reason
+'''
+A= 'Banla '
+B= 'Amar '
+C= 'Shonar '
+print(B+C+A)
+
+#Q4
+singer_name= 'Linkin Park'
+song_name= 'Numb'
+print(f'My favourite singer is {singer_name}, whose most popular song is {song_name}')
