9e96a90022894ea14abc6c09968b2d667f1c5c45
Shaikat Rahman Khan <rahmanshaikat613@gmail.com>
2025-05-17 02:09:31 +0600

    Add files via upload

---
+++ TASKS/Function task.py
@@ -0,0 +1,10 @@
+def NID(name, father, mother, present, permanent, gender, dob):
+    print(f'Name: {name}')
+    print(f'Fathers name: {father}')
+    print(f'Mothers name: {mother}')
+    print(f'Present address: {present}')
+    print(f'Permanent address: {permanent}')
+    print(f'Gender: {gender}')
+    print(f'Date of Birth: {dob}')
+
+NID('Shaikat Rahman Khan','Siddiqur Rahman Khan','Asma Akhter','Uttara','Tangail','Male','01 Dec 2001')
